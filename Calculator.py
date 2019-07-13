09b971348ad150cf96cb391867546b1f3698bd45
JenesesSandor <jeneses.sandor@gmail.com>
2019-07-13 22:46:34 +0200

    Define add function

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,16 @@
-def add(a,b):
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
     pass
 
 
-def subtract(a,b):
+def multiple(a, b):
     pass
 
 
-def multiple(a,b):
-    pass
-
-
-def divide(a,b):
+def divide(a, b):
     pass
 
 
