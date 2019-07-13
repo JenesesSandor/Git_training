ec44a4f34193e936e9ac1b81fd35633913bb0358
JenesesSandor <jeneses.sandor@gmail.com>
2019-07-13 21:13:20 +0200

    Skeleton of calculator program

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    pass
+
+
+def subtract(a,b):
+    pass
+
+
+def multiple(a,b):
+    pass
+
+
+def divide(a,b):
+    pass
+
+
+first_num = int(input("Give first number:"))
+second_num =int(input("Give second number:"))
